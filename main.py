e7c6e1e78fc310a079631cf29cccc376fe68b1e2
Gilad Grant <ggrant966@gmail.com>
2020-08-28 19:03:40 +0100

    feat: add timer and scrape amount message

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/Users/giladgrant/Documents/Code/Automation/tweet-finder/bin/python3
+import time
 from src.twitter_search import TwitterSearch
 from src.file_parser import FileParser
 from src.exporter import Exporter
@@ -8,14 +9,21 @@ def main():
     twitter = TwitterSearch()
     queries = FileParser("./queries.txt").queries
     exporter = Exporter('results')
+    amount = 250
+
+    # Start time
+    tic = time.perf_counter()
 
     # Search
-    twitter.search(queries)
+    twitter.search(queries, amount)
     results = twitter.results
+
+    # End time
+    toc = time.perf_counter()
+    print(f'Scraped {len(queries) * amount} tweets in: {toc - tic:0.2f} seconds')
 
     # Export results
     exporter.export(results)
-
 
 if __name__ == "__main__":
     main()
